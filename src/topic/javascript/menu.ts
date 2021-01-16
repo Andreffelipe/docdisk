f605189749055cc05cbb9bd8708bb49460254961
Andreffelipe <felipe.andre35@yahoo.com.br>
2021-01-16 10:55:59 -0300

    update versão

--- src/topic/javascript/menu.ts
+++ src/topic/javascript/menu.ts
@@ -1,22 +1,21 @@
 const menuJavascript = `
     Javascript Topics
 1. basic
-2. variaveis
-3. tipos de dados
-4. objetos
-5. array
-6. operadores
-7. funcoes
-8. saida de dados
-9. funcoes globais
-10. loops e fluxo
-11. caracteres de fuga
-12. string
-13. expressao regular
-14. number
-15. datas
-16. DOM
-17. erros
-18. reference
+2. tipos de dados
+3. objetos
+4. array
+5. operadores
+6. funcoes
+7. saida de dados
+8. funcoes globais
+9. loops e fluxo
+10. caracteres de fuga
+11. string
+12. expressao regular
+13. number
+14. datas
+15. DOM
+16. erros
+17. reference
 ==> `;
 export default menuJavascript;
